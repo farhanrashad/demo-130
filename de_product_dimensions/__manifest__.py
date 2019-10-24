37598c77170464742993d3a60258893bfab46ad7
farhanrashad <farhan@dynexcel.com>
2019-10-24 20:18:11 +0000

    add dimensions

--- de_product_dimensions/__manifest__.py
+++ de_product_dimensions/__manifest__.py
@@ -20,13 +20,12 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','product','stock'],
+    'depends': ['base','product','sale','stock','purchase'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
-        'views/stock_move_views.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
--- de_product_dimensions/models/.ipynb_checkpoints/models-checkpoint.py
+++ de_product_dimensions/models/.ipynb_checkpoints/models-checkpoint.py
@@ -5,21 +5,11 @@ from odoo import models, fields, api
 class Product(models.Model):
     _inherit = 'product.template'
 
-    length = fields.Integer(string='Length',)
-    width = fields.Integer(string='Width',)
-    height = fields.Integer(string='Height',)
-    gsm = fields.Integer(string='GSM',)
+    weight_sm = fields.Integer(string='GSM')
+    width = fields.Integer(string='Width')
+    length = fields.Integer(string='Length')
+    height = fields.Integer(string='Height')
+    weight1 = fields.Integer(string='Weight')
     
-    secondary_unit_qty = fields.Float(string='Unit Quantity',default=1.0,required=True)
-
-    #secondary_uom_id = fields.Many2one( 'uom.uom', string='Secondary (UOM)', stored=True, help="Default Unit of Measure used for dimension.")
-    secondary_uom_id = fields.Many2one('uom.uom', 'Secondary UOM', required=True, Help="Alternative or secondary Unit of measurement")
-    secondary_qty_available = fields.Float(string="Secondary Unit of Measure", compute='get_secondary_qty_available')
     
-    @api.depends('secondary_uom_id')
-    def get_secondary_qty_available(self):
-        for record in self:
-            record.secondary_qty_available = record.qty_available
-            if record.secondary_uom_id:
-                record.secondary_qty_available = record.qty_available * record.secondary_unit_qty
     
