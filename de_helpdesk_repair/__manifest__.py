78332b026db282c6243bec9b17c0e4af2154d670
farhanrashad <farhan@dynexcel.com>
2019-11-30 18:20:05 +0000

    add helpdesk repair

--- de_helpdesk_repair/__manifest__.py
+++ de_helpdesk_repair/__manifest__.py
@@ -15,11 +15,11 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Project',
+    'category': 'After-Sales',
     'version': '1.0',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','stock','helpdesk_mgmt','repair'],
+    'depends': ['base','stock','de_helpdesk','repair'],
 
     # always loaded
     'data': [
