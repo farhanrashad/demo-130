8fb92666854076f73f3b3f0a742a68f90469cb0c
farhanrashad <farhan@dynexcel.com>
2020-02-18 10:17:58 +0000

    update site

--- de_helpdesk_site/models/.ipynb_checkpoints/helpdesk_site-checkpoint.py
+++ de_helpdesk_site/models/.ipynb_checkpoints/helpdesk_site-checkpoint.py
@@ -5,8 +5,10 @@ from odoo import models, fields, api
 class HelpdeskSite(models.Model):
     _inherit = "helpdesk.ticket"
 
+    distributor = fields.Char('Distributor', index=True)
     site_name = fields.Char('Site Name', index=True)
     contact_name = fields.Char('Contact Name')
+    contact_no = fields.Char('Contact no')
     site_address = fields.Char('Site Address')
     region = fields.Char('Region')
     city = fields.Char('City')
@@ -16,3 +18,7 @@ class HelpdeskSite(models.Model):
     material = fields.Char('Material')
     barcode = fields.Char('Barcode/Serial')
     model = fields.Char('Model')
+    
+    customer_model_desc = fields.Char('Customer Model Desc')
+    reason = fields.Text('Model')
+    crmid = fields.Char('CRM ID')
