140c87bd3408474183606b12b23b6dabf8f1b4ac
Ali Muhammad Afzal <52150966+aleeraii@users.noreply.github.com>
2020-02-27 13:54:11 +0500

    version 2
    change manifest file. maintain version , description and company info

--- de_sale_approval/__manifest__.py
+++ de_sale_approval/__manifest__.py
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "de_approval_state",
+    'name': "Sale Order Approval",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
+    Sale Order Approval
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'description': """
+        Sale Order Approval
+    """,
+
+    'author': "Dynexcel",
+    'website': "http://www.dynexcel.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Sale',
+    'version': '0.2',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'sale'],
