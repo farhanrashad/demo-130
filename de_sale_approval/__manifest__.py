4b6933d450e6c63025c773b861a8a56717c034e6
Ali Muhammad Afzal <52150966+aleeraii@users.noreply.github.com>
2020-02-27 15:28:30 +0500

    Revert "version 2"
    This reverts commit 140c87bd3408474183606b12b23b6dabf8f1b4ac.

--- de_sale_approval/__manifest__.py
+++ de_sale_approval/__manifest__.py
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Sale Order Approval",
+    'name': "de_approval_state",
 
     'summary': """
-    Sale Order Approval
-    """,
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
 
     'description': """
-        Sale Order Approval
+        Long description of module's purpose
     """,
 
-    'author': "Dynexcel",
-    'website': "http://www.dynexcel.com",
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Sale',
-    'version': '0.2',
+    'category': 'Uncategorized',
+    'version': '0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'sale'],
