9a0b4f4ff5926b84542826f19d8f5028010b9bb9
farhanrashad <farhan@dynexcel.com>
2020-03-03 13:57:03 +0000

    update position

--- de_sale_fields_position/models/.ipynb_checkpoints/sale_order-checkpoint.py
+++ de_sale_fields_position/models/.ipynb_checkpoints/sale_order-checkpoint.py
@@ -13,7 +13,7 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
     
     entry_date = fields.Date('Created Date', compute='_compute_manual_dates', readonly=True)
-    delivery_date = fields.Date('Created Date', compute='_compute_manual_dates',readonly=True)
+    delivery_date = fields.Date('Delivery Date', compute='_compute_manual_dates',readonly=True)
     
     def _compute_manual_dates(self):
         for line in self:
