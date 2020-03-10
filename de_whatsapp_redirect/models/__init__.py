5174b81b57ffa9b70259b6e3066b06506ff11c49
ramzannoon <57062791+ramzannoon@users.noreply.github.com>
2020-03-10 17:37:42 +0500

    unnecessary

--- de_whatsapp_redirect/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import de_whatsapp_invoicing_customer
-from . import de_whatsapp_inventory_stock_picking
-from . import de_whatsapp_sale_order
-from . import de_whatsapp_purchase_order
--- de_whatsapp_redirect/wizard/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import invoicing_wizard
-from . import stock_picking
-from . import sale_order
