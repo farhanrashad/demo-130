100aeb34fbabec1db98156fb283366bb1438c2bc
ramzannoon <57062791+ramzannoon@users.noreply.github.com>
2020-03-10 13:03:42 +0500

    all word in Camel Case

--- de_sale_order_status/models/sale_order_status.py
+++ de_sale_order_status/models/sale_order_status.py
@@ -3,7 +3,7 @@ from odoo import models, fields, api, _
 class Saleorderstatus(models.Model):
     _inherit = 'sale.order'
 
-    sale_order_status = fields.Selection([('in-process', 'In-process'),
+    sale_order_status = fields.Selection([('in-process', 'In Process'),
                                     ('shipped', 'Shipped'),
                                     ('pending', 'Pending'),
                                     ('cancelled', 'Cancelled'),
