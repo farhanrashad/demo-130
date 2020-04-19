fefba2d687ccce2f4c918ebbbb1e3c8ceab55605
ramzannoon <ramzannoon675@gmail.com>
2020-04-19 03:01:42 +0000

    update account sale and purchase register

--- de_account_invoice_register/__manifest__.py
+++ de_account_invoice_register/__manifest__.py
@@ -11,10 +11,16 @@
     'name': "Invoice Register",
     'author': 'Dynexcel',
     'category': 'account',
-    'summary': """Report for customer's outstanding invoice amount within the particular date period""",
+    'summary': """ Update Account Register
+                   1- Sale Register
+                   2- Purchase Register
+    """,
     'website': 'http://www.dynexcel.com',
     'license': 'AGPL-3',
     'description': """
+                Update Account Register for GMSA to print report
+                1-Invoices which product_id true.
+                2-multiple tax on single invoice product.
 """,
     'version': '1.0',
     'depends': ['base','account'],
