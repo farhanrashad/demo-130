790d00748a874eb416b7e4d93f3c3a3090b607e6
dynexcel-ddc1 <ddc1@dynexcel.co>
2020-07-23 12:10:55 +0500

    removed

--- de_equipments_maintenance/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import res_company
-from . import equipment
-from . import maintenance_order
-from . import maintenance_request
-from . import stock_picking
-from . import stock_move
