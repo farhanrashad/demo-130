599514c741ceac6eedc606019a7afaa25fe6b946
dynexcel-ddc1 <ddc1@dynexcel.co>
2020-08-07 23:14:13 +0500

    salr ref added

--- de_mrp_saleref/models/mrp.py
+++ de_mrp_saleref/models/mrp.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+from . import config
+from . import update
 
 from collections import defaultdict
 from dateutil.relativedelta import relativedelta
@@ -12,12 +14,17 @@ from odoo.osv import expression
 class StockRule(models.Model):
     _inherit = 'stock.rule'
 
+    #     list1 = []
+    #     list1.append(origin)
+
     def _prepare_mo_vals(self, product_id, product_qty, product_uom, location_id, name, origin, company_id, values,
                          bom):
         date_deadline = fields.Datetime.to_string(self._get_date_planned(product_id, company_id, values))
+        config.list12.append(origin)
+
         return {
             'origin': origin,
-            'sale_id': name,
+            'sale_id': config.list12[0],
             'product_id': product_id.id,
             'product_qty': product_qty,
             'product_uom_id': product_uom.id,
@@ -43,21 +50,22 @@ class MrpProduction(models.Model):
     _inherit = 'mrp.production'
 
     sale_id = fields.Char(string='Ref Sale')
-    
+
     # @api.depends('origin')
     # def _compute_sale_ref(self):
     #     for list in self:
     #         test = self.env['sale.order'].search([('name','=', list.origin)], limit=1)
     #         list.sale_id = test.name
-        
-    
+
+
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
-    
-#     def action_confirm(self):
-#         res = super(SaleOrder, self).action_confirm()
-#         vals = {
-#             'sale_id': self.name,
-#         }
-#         test = self.env['mrp.production'].write(vals)
-#         return res
+
+    def action_confirm(self):
+        #         vals = {
+        #             'sale_id': self.name,
+        #         }
+        #         test = self.env['mrp.production'].write(vals)
+        res = super(SaleOrder, self).action_confirm()
+        config.list12 = []
+        return res
