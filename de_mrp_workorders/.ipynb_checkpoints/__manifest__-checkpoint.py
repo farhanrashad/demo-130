a1d8f6a392242854abce132fcd031a051baab810
dynexcel-ddc1 <ddc1@dynexcel.co>
2020-08-20 12:50:43 +0500

    work order updated

--- de_mrp_workorders/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ de_mrp_workorders/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Work Orders",
+    'name': "Work order",
 
     'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
 
     'description': """
@@ -19,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','mrp'],
+    'depends': ['base','mrp','sale','product','purchase'],
 
     # always loaded
     'data': [
