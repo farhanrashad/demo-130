8a7bb448764880ba90ec4e87f98fb3f7faadaf2a
dynexcel-ddc1 <ddc1@dynexcel.co>
2020-09-04 11:49:55 +0000

    update partner

--- de_partner_modification/__manifest__.py
+++ de_partner_modification/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','sale','purchase','account','stock'],
+    'depends': ['base','sale','purchase','account','stock','mail'],
 
     # always loaded
     'data': [
