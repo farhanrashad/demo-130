a2f261201bae73263f6995d3b5499b3b763f5cc9
farhanrashad <farhan@dynexcel.com>
2020-09-06 18:57:15 +0000

    add hr functionality

--- de_employee_performance/models/project.py
+++ de_employee_performance/models/project.py
@@ -6,7 +6,7 @@ from odoo import models, fields, api
 class ProjectTask(models.Model):
     _inherit = 'project.task'
 
-    task_kra_line = fields.One2many('project.task.kra.line', 'task_id', string='Task Kra Lines', )
+    task_kra_line = fields.One2many('hr.employee.kra.line', 'task_id', string='Task Kra Lines', )
     kra_total_score = fields.Float(compute='_compute_kra_score_total', string='Total Score', readonly=True, store=True)
 
     @api.depends('task_kra_line.score')
@@ -23,15 +23,28 @@ class ProjectTask(models.Model):
             })
 
             
-class KraLine(models.Model):
-    _name = 'project.task.kra.line'
+class EmpoyeeKraLine(models.Model):
+    _name = 'hr.employee.kra.line'
     _description = 'Task KRA Line'
     
-    task_id = fields.Many2one('project.task', string='Task Reference', required=True, ondelete='cascade', index=True, copy=False)
-    kra_id = fields.Many2one('hr.kra', string='Kra', required=True, domain="[('is_task', '=', True)]", change_default=True, ondelete='restrict')
+    task_id = fields.Many2one('project.task', string='Task Reference', required=False, ondelete='cascade', index=True, copy=False)
+    kra_id = fields.Many2one('hr.kra', string='Kra', required=True, change_default=True, ondelete='restrict')
     kra_line_id = fields.Many2one('hr.kra.line', required=False, string='Result', domain="[('kra_id', '=', kra_id)]", change_default=True, ondelete='restrict')
     score = fields.Float(related='kra_line_id.score', string='Score', store=True,readonly=True)
     name = fields.Char(string='Description')
+    employee_id = fields.Many2one('hr.employee', "Employee", compute='_get_user_id', required=True, readonly=False, store=True,)
+    user_id = fields.Many2one('res.users',"User", compute='_get_user_id', readonly=True, store=True,)
+    manager_id = fields.Many2one('res.users', string='Manager', index=True, tracking=2, readonly=True, default=lambda self: self.env.user,)
+    kra_date = fields.Date(string='Date', required=True, readonly=True, index=True, copy=False, default=fields.Date.today(), )
+
+    
+    @api.depends('task_id','kra_id','task_id.user_id')
+    def _get_user_id(self):
+        for line in self:
+            line.update({
+                'user_id': line.task_id.user_id.id,
+                'employee_id': line.task_id.user_id.employee_id.id,
+            })
     
     
     
