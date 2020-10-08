1fb1a6cd14e08e1b5f098009c8556fb4a82ea18f
dynexcel-ddc1 <ddc1@dynexcel.co>
2020-10-08 11:46:37 +0500

    update stock negative

--- de_stock_stockpicking_negative/models/stock_picking_ext.py
+++ de_stock_stockpicking_negative/models/stock_picking_ext.py
@@ -20,12 +20,17 @@ class DisableNegativeStock(models.Model):
                 for bom in bom_obj:
                     if bom.type == 'subcontract':
                         delivery_orders = self.env['stock.picking'].search([('origin', '=', self.name)])
+                        print('delivery_orders', delivery_orders)
                         for order in delivery_orders:
+                            print('order', order, order.state)
                             if order.state != 'done':
                                 raise UserError(_('Please make sure you have sent the required '
                                                   'products to your subcontractor.'))
-                            else:
+                            elif order.state == 'done':
                                 return super(DisableNegativeStock, self).button_validate()
+                            else:
+                                print('NIL')
+                            #     return super(DisableNegativeStock, self).button_validate()
                     else:
                         return super(DisableNegativeStock, self).button_validate()
         else:
