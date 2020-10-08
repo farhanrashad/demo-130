63480743799c7f85ead40d8060f609ec2278b066
dynexcel-ddc1 <ddc1@dynexcel.co>
2020-10-08 05:06:09 +0000

    update cost

--- de_mrp_cost/models/direct_cost.py
+++ de_mrp_cost/models/direct_cost.py
@@ -12,6 +12,19 @@ class ProductTemplate(models.Model):
 class MrpProduction(models.Model):
     _inherit = 'mrp.production'
     
+    def view_bill_button(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'binding_type': 'action',
+            'multi': False,
+            'name': 'Vendor Bill',
+            'domain': [('invoice_origin','=', self.name)],
+            'target': 'current',
+            'res_model': 'account.move',
+            'view_mode': 'tree,form',
+        }
+    
     def button_generate_bill(self):
         vendor_list = []
         for line in self.cost_lines:
@@ -34,14 +47,7 @@ class MrpProduction(models.Model):
                             'product_uom_id': line.product_id.uom_po_id.id,
                         }
                         product_list.append(valss)
-#             vals = {
-#                   'partner_id': partner.id,
-#                   'journal_id': self.journal_id.id,
-#                   'invoice_date': fields.Date.today(),
-#                   'type': 'out_invoice',
-#                   'invoice_origin': self.id,
-#                     }
-#             move = self.env['account.move'].create(vals)
+                        
             move_dict = {
                   'partner_id': partner.id,
                   'journal_id': self.journal_id.id,
@@ -50,63 +56,23 @@ class MrpProduction(models.Model):
                   'invoice_origin': self.name,
                    }
                
-            line_ids = []
-            debit_sum = 0.0
-            credit_sum = 0.0
-            sum_tot_amount = 0.0
-            
-                        #step2:debit side entry
-            for oline in self.cost_lines:
-                sum_tot_amount = sum_tot_amount + oline.standard_price 
-                debit_line = (0, 0, {
-    #                  	'move_id': move.id,
-                    'name': oline.product_id.name,
-                    'debit': abs(oline.standard_price),
-                    'credit': 0.0,
-                    'account_id': oline.account_id.id,
-                         })
-                line_ids.append(debit_line)
-                debit_sum += debit_line[2]['debit'] - debit_line[2]['credit']
 
-                #step3:credit side entry
-            credit_line = (0, 0, {
-                  'name': self.name,
-                  'debit': 0.0,
-                  'credit': abs(sum_tot_amount),
-                  'account_id': self.credit_account_id.id,
-                          })
-            line_ids.append(credit_line)
-            credit_sum += credit_line[2]['credit'] - credit_line[2]['debit']
-            
             move_dict['invoice_line_ids'] = product_list
-#             move_dict['line_ids'] = line_ids
-
             move = self.env['account.move'].create(move_dict)
-
-        
-#             for product in product_list:
-#                 move_line = {
-#                        'move_id': move.id,
-#                        'product_id': product['product_id'],
-#                        'name': product['name'],
-#                        'account_id': product['account_id'],
-#                        'price_unit': product['price_unit'],
-#                        'partner_id': product['partner_id'],
-                        
-#                        'product_uom_id': product['product_uom_id'],
-#                         }
-#                 orders_lines = self.env['account.move.line'].create(move_line)
-#         self.partner_id= False
-
         for line in self.cost_lines:
             if line.is_billed == False and line.partner_id:
                 line.update ({
-#                    'po_process': False,
                     'is_billed': True,
-                  	})    
+                  	})
+                
+                
+    def get_document_count(self):
+        count = self.env['account.move'].search_count([('invoice_origin','=', self.name)])
+        self.document_id = count
     
-    credit_account_id = fields.Many2one('account.account', string='Credit Account', required=True)    
-    journal_id = fields.Many2one('account.journal', string='Journal', required=True)
+    document_id = fields.Integer(compute='get_document_count')
+#     credit_account_id = fields.Many2one('account.account', string='Credit Account', required=True)    
+    journal_id = fields.Many2one('account.journal', string='Journal', required=True, )
     cost_lines = fields.One2many('mrp.production.direct.cost', 'production_id' ,string='Direct Cost Lines')    
     
     
@@ -115,7 +81,7 @@ class MrpCost(models.Model):
     _description = 'This Production Order Cost'
 
     product_id = fields.Many2one('product.product',string='Product', domain="[('type', '=', 'service')]")
-    account_id = fields.Many2one('account.account', string='Account', required=True)
+    account_id = fields.Many2one('account.account', string='Account', required=True, )
     production_id = fields.Many2one('mrp.production', string="Manufacturing Order")
     is_charge = fields.Boolean(related='product_id.is_charge')
     standard_price = fields.Float(related='product_id.standard_price', readonly=False)
