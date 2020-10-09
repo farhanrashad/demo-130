536ee565e71944ad5561a92e1f97896a06ca6ae6
Abdul Haseeb <a.haseeb@dynexcel.co>
2020-10-09 10:24:36 +0500

    de_unique_reference

---
+++ de_unique_reference/de_unique_reference/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "Unique Reference Field in Invoicing",
+    "category": 'Invoicing',
+    "summary": 'Unique_ref',
+    "description": """
+	 
+   
+    """,
+    "sequence": 3,
+    "author": "Dynexcel",
+    "website": "http://www.dynexcel.co",
+    "version": '13.1.0.0',
+    "depends": ['base','account'],
+    "data": [
+        'security/ir.model.access.csv',
+    ],
+
+    "price": 25,
+    "currency": 'EUR',
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
