b60689c57e9314395f31ddc4f38b2f812e17fdda
dynexcel-ddc1 <ddc1@dynexcel.co>
2020-11-18 16:00:42 +0500

    de_construction

--- de_construction/wizards/con_create_sub_tasks.py
+++ de_construction/wizards/con_create_sub_tasks.py
@@ -1,9 +1,10 @@
 from odoo import api, fields, models
 
-
 class CreateSubtsk(models.Model):
     _name = 'create.subtask'
     _description = 'Create SubTask'
+
+
 
     user_id = fields.Many2one(comodel_name="res.users", string="", required=False, )
     task_id = fields.Char(string="", required=False, )
