0bc07c1c07006b13ef4da508e611c9d7f65dca84
dynexcel-ddc1 <ddc1@dynexce.co>
2020-12-10 08:20:29 +0500

    update

--- de_account_balance/models/account_balance.py
+++ de_account_balance/models/account_balance.py
@@ -4,6 +4,9 @@ from datetime import datetime
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError, ValidationError, Warning
 
+
+
+
 class AccountBalance(models.Model):
     _inherit = 'res.partner'
 
