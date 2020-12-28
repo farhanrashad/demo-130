2cd84870a6f2ba03a6116e573bd452d3ad9e0512
dynexcel-ddc1 <ddc1@dynexce.co>
2020-12-28 18:41:35 +0500

    update

--- de_whatsapp_connector/wizard/__init__.py
+++
@@ -1,7 +0,0 @@
-
-from . import message_wizard
-from . import whatsapp_message_wizard
-from . import whatsapp_message_wizard_payment
-from . import whatsapp_message_wizard_employee
-from . import whatsapp_message_wizard_stock
-
