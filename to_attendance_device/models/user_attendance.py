f3c80fdaec7088d1dd5760f2622dda96d663a185
dynexcel-ddc1 <ddc1@dynexce.co>
2021-01-01 18:08:45 +0500

    update attendance

--- to_attendance_device/models/user_attendance.py
+++ to_attendance_device/models/user_attendance.py
@@ -67,30 +67,30 @@ class UserAttendance(models.Model):
         return attendances
 
     def action_attendace_validated(self):
-        # for month_date in range(32):
-        datetime = fields.date.today() - timedelta(days=14)
-        date_start = datetime + relativedelta(hours =+ 1)
-        date_end = datetime + relativedelta(hours =+ 23)
-        total_employee = self.env['hr.employee'].search([])
-        for employee in total_employee:
-            attendance_test = self.env['user.attendance']
-            count = attendance_test.search_count([('employee_id','=',employee.id)])
-            if count > 1:
-                attendance_checkin = attendance_test.search([('employee_id','=',employee.id),('timestamp','>=',date_start),('timestamp','<=',date_end),('is_attedance_created','=',False)], order="timestamp asc", limit=1)
-                attendance_checkout = attendance_test.search([('employee_id','=',employee.id),('timestamp','>=',date_start),('timestamp','<=',date_end),('is_attedance_created','=',False)], order="timestamp desc", limit=1)
-                if attendance_checkin and attendance_checkout:
-                    vals = {
-                           'employee_id': attendance_checkin.employee_id.id,
-                           'check_in': attendance_checkin.timestamp,
-                           'check_out': attendance_checkout.timestamp,
-                              }
-                    hr_attendance = self.env['hr.attendance'].create(vals)
+        for month_date in range(32):
+            datetime = fields.date.today() - timedelta(month_date)
+            date_start = datetime + relativedelta(hours =+ 1)
+            date_end = datetime + relativedelta(hours =+ 23)
+            total_employee = self.env['hr.employee'].search([])
+            for employee in total_employee:
+                attendance_test = self.env['user.attendance']
+                count = attendance_test.search_count([('employee_id','=',employee.id)])
+                if count > 1:
+                    attendance_checkin = attendance_test.search([('employee_id','=',employee.id),('timestamp','>=',date_start),('timestamp','<=',date_end),('is_attedance_created','=',False)], order="timestamp asc", limit=1)
+                    attendance_checkout = attendance_test.search([('employee_id','=',employee.id),('timestamp','>=',date_start),('timestamp','<=',date_end),('is_attedance_created','=',False)], order="timestamp desc", limit=1)
+                    if attendance_checkin and attendance_checkout:
+                        vals = {
+                               'employee_id': attendance_checkin.employee_id.id,
+                               'check_in': attendance_checkin.timestamp,
+                               'check_out': attendance_checkout.timestamp,
+                                  }
+                        hr_attendance = self.env['hr.attendance'].create(vals)
 
-        attendancelist = attendance_test.search([('employee_id','=',employee.id),('timestamp','>=',date_start),('timestamp','<=',date_end),('is_attedance_created','=',False)])
-        for line in attendancelist:
-            line.update({
-               'is_attedance_created' : True
-             })           
+            attendancelist = attendance_test.search([('employee_id','=',employee.id),('timestamp','>=',date_start),('timestamp','<=',date_end),('is_attedance_created','=',False)])
+            for line in attendancelist:
+                line.update({
+                   'is_attedance_created' : True
+                 })
                     
 
 
