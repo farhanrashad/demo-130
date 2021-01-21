50a2372934c681ec3471b46369826185daad26f8
dynexcel-ddc1 <ddc1@dynexcel.co>
2021-01-21 12:08:40 +0000

    updated By Musadiq Helpdesk Two Fields

--- de_helpdesk_repair_mgmt_extend/models/models.py
+++ de_helpdesk_repair_mgmt_extend/models/models.py
@@ -5,13 +5,13 @@ from odoo import models, fields, api
 
 class HelpdeskExtendInherit(models.Model):
     _inherit = 'helpdesk.ticket.team'
-    _description = 'This table handle the data of helpdesk'
 
-    technician_ids = fields.Many2one('res.users', string='Technician')
+    technician_ids = fields.Many2many('res.users','helpdesk_ticket_teams_res_users_rel', string='Technician')
 
 
 class HelpdeskExtends(models.Model):
     _inherit = 'helpdesk.ticket'
-    _description = 'This table handle the data of helpdesk tickets'
-
-    technician_id = fields.Many2one('res.users',string='Technician')
+    
+    team = fields.Many2many(related='team_id.technician_ids', string='rel', invisible=True)
+    
+    technician_id = fields.Many2one('res.users',string='Technician', domain="[('id','in',team)]")
