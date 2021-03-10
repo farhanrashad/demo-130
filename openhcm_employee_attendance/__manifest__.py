8e83b4bdd43083224bc3b6ae7cdae3a4435961af
usamashakeel51435 <79131226+usamashakeel51435@users.noreply.github.com>
2021-03-10 14:21:45 +0500

    Add files via upload

---
+++ openhcm_employee_attendance/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Openhcm Employee Attendance",
+    'version': '14.0.0.0',
+    'category': 'Employee',
+    'summary': 'Openhcm Employee Attendance ',
+    'sequence': 3,
+    'description': """"  """,
+    'author': "Dynexcel",
+    'website': "http://www.dynexcel.co",
+#     'license': 'LGPL-3',
+    'depends': ['base'],
+    
+    'data': [
+#         'views/account_move.xml',
+    ],
+
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
