cd0820ef1ca1ad412d37d49e7c982381edefced9
usamashakeel51435 <usama.s@dynexcel.co>
2021-03-13 08:58:35 +0000

    update

--- de_gatepass_report/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ de_gatepass_report/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -1,33 +1,22 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Gatepass Report",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
-    """,
+    'summary': """ Gatepass Report """,
+    'description': """Gatepass Report for DUT""",
     "sequence": 1,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
+    'author': "Dynexcel",
+    'website': "http://www.dynexcel.com",
+    'category': 'inventorty gatepass report',
+    'version': '13.0.0.0',
+    'depends': ['base','stock'],
+    
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'security/ir.model.access.csv',
+        'wizards/gatepass_report.xml',
+        'views/gatepass_report_menu.xml',
+        
+
     ],
     # only loaded in demonstration mode
     'demo': [
