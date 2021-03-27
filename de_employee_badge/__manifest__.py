d46d4ef3f38dfb12080b3a30eaa29708e52b1579
usama51435 <usama.s@dynexcel.co>
2021-03-27 11:43:57 +0500

    employee badge

---
+++ de_employee_badge/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Employee badge Print",
+    'version': '14.0.0.0',
+    "category": 'Employee badge Print',
+    "summary": ' Employee badge Print',
+    'sequence': 7,
+    "description": """"  """,
+    "author": "Dynexcel",
+    "website": "http://www.dynexcel.co",
+    'license': 'LGPL-3',
+    'depends': ['base'],
+    'data': [
+
+
+        #'report/gtn_report.xml',
+        #'views/transfer_tab.xml',
+
+    ],
+
+  
+}
+
