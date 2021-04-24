f4542e1a57f64fc8b41142d7957456ed0ebd96ec
Abdullah Akhtar <79564215+abdulakhtardynexcel@users.noreply.github.com>
2021-04-24 09:24:15 +0500

    de_po_enhancement

---
+++ de_po_enhancement/wizards/po_enhancement_wizard.py
@@ -0,0 +1,44 @@
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+
+
+class PurchaseOrderWizard(models.Model):
+    _name = "purchase.enhancement"
+    _description = "PO Wizard"
+
+    products_po = fields.Char(string='Products')
+    demand_po = fields.Float(string='Demand')
+    quantity_po = fields.Float(string='Quantity Done')
+    purchase_order = fields.One2many('purchase.enhancement.line', 'purchase_order_ids')
+    po_line_id = fields.Many2one('purchase.order.line')
+    po_line_ref = fields.Integer()
+
+    @api.model
+    def create(self, vals):
+        res = super(PurchaseOrderWizard, self).create(vals)
+        res.po_line_id = res.po_line_ref
+        res.po_line_id.check_id = res.id
+        #         raise UserError(res.po_line_id.id)
+        return res
+
+
+class PurchaseOrderWizardLine(models.Model):
+    _name = "purchase.enhancement.line"
+    _description = "PO Wizard Line"
+
+    purchase_order_ids = fields.Many2one('purchase.enhancement')
+
+    sale_order_po = fields.Many2one('sale.order', string='SO')
+    quantity = fields.Char(string='Quantity')
+
+# class PurchaseOrderLineInherit(models.Model):
+#     _inherit = 'purchase.order.line'
+
+#     @api.onchange('products_po', 'quantity_po')
+#     def get_quantity(self):
+#         model = self.env.context.get('active_model')
+#         rec = self.env[model].browse(self.env.context.get('active_id'))
+#         for line in rec:
+#             line.product_id = self.products_po
+
+#             line.product_qty = self.quantity_po
---
+++ de_po_enhancement/wizards/stock_enhancement_wizard.py
@@ -0,0 +1,50 @@
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+
+
+class StockPickingWizard(models.Model):
+    _name = "stock.enhancement"
+    _description = "Stock Picking Wizard"
+
+    products_sp = fields.Many2one('product.product', 'Products')
+    demand_sp = fields.Float(string='Demand')
+    quantity_sp = fields.Float(string='Quantity Done')
+    stock_pick = fields.One2many('stock.enhancement.line', 'stock_order_ids')
+    ps_line_id = fields.Many2one('stock.move')
+    ps_line_ref = fields.Integer()
+
+    @api.model
+    def create(self, vals):
+        res = super(StockPickingWizard, self).create(vals)
+        res.ps_line_id = res.ps_line_ref
+        res.ps_line_id.check_id_stock = res.id
+        res.products_sp = res.ps_line_id.product_id.id
+        for line in res.stock_pick:
+            #             for sale in line.sale_order_sp:
+            delivery = self.env['stock.picking'].search([('origin', '=', line.sale_order_sp.name)])
+            for dell in delivery.move_ids_without_package:
+                if dell.product_id.id == res.products_sp.id:
+                    dell.test = line.quantity
+        return res
+
+
+class StockPickingWizardLine(models.Model):
+    _name = "stock.enhancement.line"
+    _description = "Stock Picking Wizard Line"
+
+    stock_order_ids = fields.Many2one('stock.enhancement')
+
+    sale_order_sp = fields.Many2one('sale.order', string='SO')
+    quantity = fields.Char(string='Quantity')
+
+# class StockMoveLineInherit(models.Model):
+#     _inherit = 'stock.move'
+
+#     @api.onchange('products_sp', 'quantity_sp')
+#     def get_quantity(self):
+#         model = self.env.context.get('active_model')
+#         rec = self.env[model].browse(self.env.context.get('active_id'))
+#         for line in rec:
+#             line.product_id = self.products_sp
+
+#             line.product_qty = self.quantity_sp
