1db859ab2c6b41a2f6f7bd4420869b79ebf33e94
Abdullah Akhtar <79564215+abdulakhtardynexcel@users.noreply.github.com>
2021-04-26 04:20:32 +0500

    de_purchase_agreement_approvals

---
+++ de_purchase_agreement_approvals/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Agreement Approvals",
+    'summary': """Purchase Agreement Approvals""",
+    'description': """Purchase Agreement Approvals""",
+    'author': "Dynexcel",
+    'website': "https://www.dynexcel.com",
+    'category': 'Human Resource',
+    'version': '14.0.0.1',
+    'depends': ['base', 'approvals', 'purchase'],
+    'data': [
+        'views/approval_request_views.xml',
+    ],
+}
