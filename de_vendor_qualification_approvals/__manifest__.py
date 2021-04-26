06ba973c3298a5824edb3a6a3d990e9ec19bfeec
Abdullah Akhtar <79564215+abdulakhtardynexcel@users.noreply.github.com>
2021-04-26 09:53:28 +0500

    de_vendor_qualification_approvals

---
+++ de_vendor_qualification_approvals/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Vendor Qualification Approvals",
+    'summary': """Vendor Qualification Approvals""",
+    'description': """Purchase Agreement Approvals""",
+    'author': "Dynexcel",
+    'website': "https://www.dynexcel.com",
+    'category': 'Vendor',
+    'version': '14.0.0.1',
+    'depends': ['base', 'approvals', 'inventory'],
+    'data': [
+        'views/approval_request_views.xml',
+    ],
+}
