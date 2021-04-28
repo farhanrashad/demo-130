80431463eac4c38cceb5bd6eeea00b1d7c8861eb
Your Name <you@example.com>
2021-04-28 11:39:03 +0000

    Final Update Sale Quotation Report

--- de_sale_quotation_report/models/.ipynb_checkpoints/sale_quotation-checkpoint.py
+++ de_sale_quotation_report/models/.ipynb_checkpoints/sale_quotation-checkpoint.py
@@ -9,9 +9,4 @@ class SaleQuotation(models.Model):
     
     
     technical_representative = fields.Many2one('res.users', string="Technical Representative")
-    customer = fields.Selection(selection=[('with tax', 'With Tax'),('without tax', 'Without Tax'),], default='with tax' , string="Print As")
-#     local_vendor = fields.Boolean(string="Local", default=True)
-#     foreign_vendor = fields.Boolean(string="Foreign")
-    
-#     quotation_ref = fields.Char(string="Quotation Reference")
-    
+    customer = fields.Selection(selection=[('with tax', 'With Tax'),('without tax', 'Without Tax'),('without boq', 'Without BOQ')], default='with tax' , string="Print As")
