0f80d5f6b380f2237d9426b18dba390ce4965446
Your Name <you@example.com>
2021-05-04 04:51:16 +0000

    Updated Sale Report

--- de_sale_quotation_report/models/sale_quotation.py
+++ de_sale_quotation_report/models/sale_quotation.py
@@ -10,3 +10,4 @@ class SaleQuotation(models.Model):
     
     technical_representative = fields.Many2one('res.users', string="Technical Representative")
     customer = fields.Selection(selection=[('with tax', 'With Tax'),('without tax', 'Without Tax'),('without boq', 'Without BOQ')], default='with tax' , string="Print As")
+    notes = fields.Char("Notes")
